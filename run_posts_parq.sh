83f865931f15d3cf57731daa507c8dad958897c1
Anmol Puri <anmolpuri96@gmail.com>
2019-09-19 11:18:59 -0400

    Fixed url and added jar

--- run_posts_parq.sh
+++ run_posts_parq.sh
@@ -7,4 +7,4 @@ export PYSPARK_PYTHON=python3;
    --executor-memory 20G \
    --total-executor-cores 4 \
    --jars postgresql-9.4.1207.jar,aws-java-sdk-1.7.4.jar,hadoop-aws-2.7.3.jar \
-   /home/ubuntu/dataignition-tech/src/s3/spark/pagerank_calculation.py
+   /home/ubuntu/dataignition-tech/src/spark/posts_xml_to_parq.py
