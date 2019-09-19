96f405d4b4880d15c173ef0c72dbfa82ceb4f9b3
Anmol Puri <anmolpuri96@gmail.com>
2019-09-19 11:33:35 -0400

    Updated master IP

--- run_posts_parq.sh
+++ run_posts_parq.sh
@@ -2,7 +2,7 @@
 
 export PYSPARK_PYTHON=python3;
 /usr/local/spark/bin/spark-submit \
-   --master spark://ip-10-0-0-13:7077 \
+   --master spark://10.0.0.7:7077 \
    --executor-memory 20G \
    --total-executor-cores 4 \
    --jars postgresql-9.4.1207.jar,aws-java-sdk-1.7.4.jar,hadoop-aws-2.7.3.jar \
