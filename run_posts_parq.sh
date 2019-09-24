20b390d18a1ffd15ebc91eca69d127640fd2835e
Anmol Puri <anmolpuri96@gmail.com>
2019-09-24 11:05:02 -0400

    Uses 28G

--- run_posts_parq.sh
+++ run_posts_parq.sh
@@ -3,5 +3,5 @@
 export PYSPARK_PYTHON=python3;
 /usr/local/spark/bin/spark-submit \
    --master spark://ec2-3-228-180-18.compute-1.amazonaws.com:7077 \
-   --executor-memory 20G \
+   --executor-memory 28G \
    /home/ubuntu/dataignition-tech/src/spark/posts_xml_to_parq.py
