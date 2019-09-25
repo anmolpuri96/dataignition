58c62e6eb63d225da3b5299a7eb912d8367c336c
Anmol Puri <anmolpuri96@gmail.com>
2019-09-25 15:42:24 -0400

    Updated print statement

--- src/spark/preprocess.py
+++ src/spark/preprocess.py
@@ -138,8 +138,9 @@ def preprocess_files(bucket_name, file_name):
         rdb = redis.StrictRedis(host="ec2-52-73-233-196.compute-1.amazonaws.com", port=6379, db=0)
         for row in rdd:
             # rdb.sadd('id:{}'.format(row.id), row.min_hash)
-            print('id:{}'.format(row.id), row.min_hash)
-    #print(minhash_df.show(5, True))
+            print("SET id:{0} {1}".format(row.id, row.min_hash))
+            # print('id:{}'.format(row.id), row.min_hash)
+    print(minhash_df.show(5, True))
     minhash_df.foreachPartition(write_minhash_data_to_redis)
 
     print(colored("Finished writing minhash data to Redis", "green"))
