d96530a4de4b329f7b71a872756ce104e9df29b4
Anmol Puri <anmolpuri96@gmail.com>
2019-09-30 15:17:58 -0400

    calls udf

--- src/spark/comparison.py
+++ src/spark/comparison.py
@@ -74,8 +74,8 @@ def compare_text(overlap_threshold=0.6):
 
             minhash_ua = F.udf(lambda id: get_minhash_ua(id), ArrayType(StringType()))
             minhash_a = F.udf(lambda id: get_minhash_a(id), ArrayType(StringType()))
-            unanswered_minhash = ids_df.withColumn("unanswered_minhash", F.col("UnansweredId"))
-            answered_minhash = ids_df.withColumn("answered_minhash", F.col("AnsweredId"))
+            unanswered_minhash = ids_df.withColumn("unanswered_minhash", minhash_ua(F.col("UnansweredId")))
+            answered_minhash = ids_df.withColumn("answered_minhash", minhash_a(F.col("AnsweredId")))
 
             unanswered_minhash.show()
             answered_minhash.show()
