b799ceaaae8f03e20918b4164014b8249cd5a18e
Anmol Puri <anmolpuri96@gmail.com>
2019-10-01 18:10:41 -0400

    Uses port 5000

--- run.py
+++ run.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 from flask_app import app
-app.run(debug=True, host="ec2-52-73-233-196.compute-1.amazonaws.com", port=80)
+app.run(debug=True, host="ec2-52-73-233-196.compute-1.amazonaws.com", port=5000)
