a8d9373a7bf143652f566fef96c286f95662a431
Anmol Puri <anmolpuri96@gmail.com>
2019-10-10 14:57:00 -0400

    Reset to original http setting

--- run.py
+++ run.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 from flask_app import app
-# app.run(debug=True, host="0.0.0.0", port=80)
-app.run(ssl_context='adhoc', port=443)
-# app.run(debug=True, ssl_context=('ssl.cert', 'ssl.key'), port=443)
+app.run(debug=True, host="0.0.0.0", port=80)
