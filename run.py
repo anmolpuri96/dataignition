6051d403dbb96538fd4f7ca9ae85c0601993b62e
Anmol Puri <anmolpuri96@gmail.com>
2019-10-10 14:54:46 -0400

    Updated to port 443

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 from flask_app import app
 # app.run(debug=True, host="0.0.0.0", port=80)
-app.run(ssl_context='adhoc')
+app.run(ssl_context='adhoc', port=443)
 # app.run(debug=True, ssl_context=('ssl.cert', 'ssl.key'), port=443)
