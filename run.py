e82d08390240d4e4fe84cc2acfc657a2c11ad902
Anmol Puri <anmolpuri96@gmail.com>
2019-10-10 14:48:47 -0400

    Added debug mode

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 from flask_app import app
 # app.run(debug=True, host="0.0.0.0", port=80)
-app.run(ssl_context=('ssl.cert', 'ssl.key'), port=443)
+app.run(debug=True, ssl_context=('ssl.cert', 'ssl.key'), port=443)
