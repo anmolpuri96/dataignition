c06bbbab8a9bdf6cf71f36bcbfe74371c3f63ee0
Anmol Puri <anmolpuri96@gmail.com>
2019-10-10 15:20:26 -0400

    Imported redirect

--- flask_app/views.py
+++ flask_app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, redirect
 from flask_app import app
 # from sqlalchemy import create_engine
 # from sqlalchemy_utils import database_exists, create_database
